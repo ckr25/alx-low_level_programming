7ac2b74c4e2c09de227cde5200462eb841b16b78
ckr25 <ckipkirui208@gmail.com>
2023-02-16 11:55:45 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -4,7 +4,7 @@
  * main - prints "programming is like building a multilingual puzzle"
  * return: 0 (success)
  */
-int main(void) 
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
