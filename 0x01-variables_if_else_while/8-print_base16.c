607f70bead0bfe9cc219b325736e687bd878a225
ckr25 <ckipkirui208@gmail.com>
2023-02-17 15:00:44 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/*
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 48; /*48; decimal represantation of 0*/
+
+	while (digit <= 102) /*102; decimal rep of f*/
+	{
+		putchar(digit);
+
+		/* after 9 we skip till 96;*/
+		if (digit == 57)
+			digit += 39;
+		++digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
