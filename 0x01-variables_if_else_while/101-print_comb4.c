9d735017a072c5beff364c8015b02304f6c7e1d4
ckr25 <ckipkirui208@gmail.com>
2023-02-17 15:43:21 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -17,7 +17,7 @@ int main(void)
 			digit3 = 0;
 			while (digit3 <= 9)
 			{
-				if (digit1 != digit2 && digit1 < digi2 && digit2 != digit3 && digit2 < digit3)
+				if (digit1 != digit2 && digit1 < digit2 && digit2 != digit3 && digit2 < digit3)
 				{
 					putchar(digit1 + 48);
 					putchar(digit2 + 48);
