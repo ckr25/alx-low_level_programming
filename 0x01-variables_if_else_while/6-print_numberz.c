f999cfb05f363ce4d7844301097a3145679db262
ckr25 <ckipkirui208@gmail.com>
2023-02-17 14:39:21 -0800

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/*
+ * main - Entry level
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		/*converts digit to ASCII representation*/
+		putchar(digit + '0');
+		++ digit;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
