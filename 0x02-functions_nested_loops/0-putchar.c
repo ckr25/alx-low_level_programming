f5cb2b19e1448f869fd4cf5f5063edba91347464
ckr25 <ckipkirui208@gmail.com>
2023-02-21 11:52:55 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/*
+ * main - Entry point 
+ * prints _putchar
+ *
+ * return: Always 0 (success)
+ */
+int main(void)
+{
+	char output[] = "_putchar\n";
+	int length = sizeof(output);
+
+	for (int i = 0; i < length; i++)
+	{
+		_putchar (output[i]);
+	}
+
+	return (0);
+}
