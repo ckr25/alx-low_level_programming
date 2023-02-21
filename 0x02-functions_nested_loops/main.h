88764baf966ec22ba3c5b5974cbe0d6f4b5dc229
ckr25 <ckipkirui208@gmail.com>
2023-02-21 12:50:03 -0800

    0-putchar.c

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,13 @@
+int _putchar(char c);
+void print_alphabet(void);
+void print_alphabet x10(void);
+int _islower(int c);
+int _isalpha(int c);
+int _print_ sign(int n);
+int abs(int);
+int print_last_digit(int);
+void jack_bauer(void);
+void times_table(void);
+int add(int, int);
+int prints_to_98(int n);
+void prints_times_table(int n);
