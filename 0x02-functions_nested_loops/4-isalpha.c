b62b96da62309548c17c976f36f6dbed34f6d8d5
ckr25 <ckipkirui208@gmail.com>
2023-02-22 10:51:02 -0800

    4-isalpha.c

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ *
+ * @c - character checked
+ *
+ * Return: 0 or 1
+ */
+int _isalpha(int c)
+{
+	return ((c >= 97 && c <= 122) || (c >= 64 && (c <= 90);
+
+}			
