c7d121fb7ffdf24c5929565f3065927926da8d11
ckr25 <ckipkirui208@gmail.com>
2023-02-22 11:22:20 -0800

    5-sign.c

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_sign - check if a character is positive or negative
+ * @n - character to be checked
+ *
+ * Return: 0 or 1
+ */
+int print_ sign(int n)
+{
+	int test;
+
+	if (n > 0)
+	{
+		test = 1;
+		_putchar('+');
+	}
+
+	else if (n == 0)
+	{
+		test = 0;
+		_putchar('0');
+	}
+	else 
+	{
+		test = -1;
+		_putchar('-');
+	}
+	return (test);
+}
+				
