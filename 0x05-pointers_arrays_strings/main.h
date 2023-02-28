d149352c58aca953a98c35c447de1db985e85a28
ckr25 <ckipkirui208@gmail.com>
2023-02-28 09:35:56 -0800

    main.h

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,13 @@
+void swap_int(int *a, int *b);
+void reset_to_98(int *n);
+int _strlen(char *);
+void _puts(char *str);
+int _putchar(char c);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
+void puts_half(char *str);
+void print_array(int *a, int n);
+char *_strcpy(char *dest, char *strc);
+int _atoi(char *s);
+
