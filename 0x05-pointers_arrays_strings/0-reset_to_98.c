3a3d2354bbaade5b43c636917f2ad25055972bb9
ckr25 <ckipkirui208@gmail.com>
2023-02-28 09:42:25 -0800

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * rest_to_98 - function declaration
+ * Description - resets value to 98
+ * @n: the pointer integer
+ * Return: void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
