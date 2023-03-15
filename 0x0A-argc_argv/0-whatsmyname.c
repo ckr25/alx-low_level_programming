a76d14cf78fc74fb4a4da9e7d48cf0230881e9bf
ckr25 <ckipkirui208@gmail.com>
2023-03-15 12:30:35 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - is the function that prints the name
+ * @argc: is an argument counter for char
+ * @argv: is an argument value for char
+ * Retyrn: Always success
+ */
+int main(int argc, char *argv[])
+{
+	int i = argc - argc;
+
+	printf("%s\n", argv[i]);
+
+	return (0);
+}
