b5ca0391ff5b3bc4d5e6274b2e74282b241fab25
ckr25 <ckipkirui208@gmail.com>
2023-03-17 18:45:30 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -35,7 +35,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (k = 0; k < i; k++)
 		s[k] = s1[k];
 	for (k = 0; k < j; k++)
-		s(k + i) = s2[k];
+		s[k + i] = s2[k];
 	s[i + j] = '\0';
 	return (s);
 }
