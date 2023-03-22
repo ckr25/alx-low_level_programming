5e0a6af0b5d91e8030e533111829519b44f13375
ckr25 <ckipkirui208@gmail.com>
2023-03-22 14:58:58 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * print_name - prints a name
+ * @name: pointer to at name
+ * @f: function pointer
+ * Return: void
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
