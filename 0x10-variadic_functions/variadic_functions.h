169dcd6daedf85bc14447e54939472bdbd3c084a
ckr25 <ckipkirui208@gmail.com>
2023-03-23 05:59:54 -0700

    variadic_functions.h

---
+++ 0x10-variadic_functions/variadic_functions.h
@@ -0,0 +1,9 @@
+#ifndef VARIADIC_FUNCTIONS.H
+#define VARIADIC_FUNCTIONs.H
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+#endif
