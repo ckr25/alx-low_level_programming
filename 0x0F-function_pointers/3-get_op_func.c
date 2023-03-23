9b0f08c4eab2d7f437b8adfdfe12a1e39f1c0c55
ckr25 <ckipkirui208@gmail.com>
2023-03-22 19:06:40 -0700

    3-get_op_func.c

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,4 +1,4 @@
-#include "3-calac.h"
+#include "3-calc.h"
 #include <stdio.h>
 #include <string.h>
 /**
--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -1,4 +1,6 @@
 #include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
 /**
  * main - check arguments
  * @argc: argument count
@@ -16,7 +18,7 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	if (agrv[2][1] != '\0')
+	if (argv[2][1] != '\0')
 	{
 		printf("Error\n");
 		exit(99);
