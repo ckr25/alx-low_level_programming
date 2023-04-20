5081313f1545439313dd561024448fa4dd821e93
ckr25 <117547263+ckr25@users.noreply.github.com>
2023-04-20 09:43:44 +0300

    Delete 0x0D-preprocessor directory

--- 0x0D-preprocessor/3-function_like_macro.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIKEMACRO_H
-#define LIKEMACRO_H
-
-#define ABS(X) ((X) < 0 ? -(1) * (X) : (X))
-
-#endif
--- 0x0D-preprocessor/4-sum.h
+++
@@ -1,6 +0,0 @@
-#ifndef LIKEMACRO_H
-#define LIKEMACRO_H
-
-#define SUM(x, y) (x + y)
-
-#endif
