9214522d1f3518dc08f9e28acf4a7c3c58451d08
ckr25 <ckipkirui208@gmail.com>
2023-06-14 11:36:00 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+# include <stdio.h>
+/**
+ * main - prints the for the program
+ * @argc: is a counter for argument for int
+ * @argv: is a counter for value of the argument to char
+ * Return: Always success
+ **/
+int main(int argc, *argv[])
+{
+	(void)argv;
+
+	printf("%d\n", (argc - 1));
+	return (0);
+}
