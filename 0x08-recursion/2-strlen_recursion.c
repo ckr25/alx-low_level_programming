5d47cdaa686ffac38fc9fd1319fc277cbc9285db
ckr25 <ckipkirui208@gmail.com>
2023-08-03 10:38:47 -0700

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _strlen_recursion - main - check the code
+ * @s: character of the string
+ * Return: Always 0
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	return (0);
+}
