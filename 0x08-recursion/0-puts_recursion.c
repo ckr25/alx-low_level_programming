2b6c132499a0bcca198a8f13d385906c3b69c180
ckr25 <ckipkirui208@gmail.com>
2023-08-03 10:07:11 -0700

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - main - check the code
+ * @s: character of strings
+ * Return: Always 0
+ */
+void _puts_recursion(char *s)
+{
+	if (*(s) != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+
+	if (*(s) == '\0')
+		_putchar('\n');
+}	
