40b71cde51386f7605ec7249823d876b31daaa31
ckr25 <ckipkirui208@gmail.com>
2023-08-10 12:41:13 -0700

    101-mul.c

---
+++ 0x0C-more_malloc_free/101-mul.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+
+/**
+ * main - a programme that multiplies two positive numbers
+ * @argc: number of arguments passed
+ * @argv: an array of pointers to the argument
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	char *product;
+	int i;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[1] < 0 || argv[1] > 9)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	if (argv[2] < 0 || argv[2] > 9)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+}
