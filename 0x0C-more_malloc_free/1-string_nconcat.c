066edbc2f797101ddc9c217316f92ee2d8dbb2b9
ckr25 <ckipkirui208@gmail.com>
2023-08-10 09:04:13 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -17,7 +17,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
-		S2 = "";
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 		len++;
 	str_cat = malloc(sizeof(char) * (len + 1));
