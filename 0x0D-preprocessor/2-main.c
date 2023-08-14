3f0fbec2def115a1ede79d0a1f0582dbece40139
ckr25 <ckipkirui208@gmail.com>
2023-08-14 03:44:16 -0700

    2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - writes a program that prints the nameof the file it was compiled
+ * Return: Always 0
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
