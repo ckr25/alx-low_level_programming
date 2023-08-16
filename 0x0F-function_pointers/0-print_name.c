8c9351ddb731e65fcf15c130d270fb71e5f9eb66
ckr25 <ckipkirui208@gmail.com>
2023-08-16 09:18:29 -0700

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "function_pointer.h"
+
+/**
+ * print_name - prints a name as is
+ * @name: name of the person
+ * @f: is a pointer
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		return;
+	}
+	else
+	{
+		f(name);
+	}
+}
