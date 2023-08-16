1b2cc09d7cc1b81abd8f3286f95e2c4ba721d414
ckr25 <ckipkirui208@gmail.com>
2023-08-16 09:56:42 -0700

    2-int_index.c

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -20,7 +20,7 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	for (index = 0; index < size; index++)
 	{
-		if(cmp(array[index]))
+		if (cmp(array[index]))
 			return (index);
 	}
 	return (-1);
