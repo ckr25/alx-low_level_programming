fbf2feac77ade37eac8c1f6ca466a122b59ccea7
ckr25 <ckipkirui208@gmail.com>
2023-08-27 07:30:37 -0700

    4-free_list.c

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_list - frees a list_t list
+ * @head: contains base address of the list
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current_node;
+	
+	while (head != NULL)
+	{
+		current_node = head;
+		head = head->next;
+		free(current_node->str);
+		free(current_node);
+	}
+}
