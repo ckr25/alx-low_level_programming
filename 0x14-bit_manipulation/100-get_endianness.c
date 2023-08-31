8e736e513901949983c63ece315aa2e15ea3097f
ckr25 <ckipkirui208@gmail.com>
2023-08-31 13:04:27 -0700

    100-get_endianness.c

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks the endianness
+ *
+ * Retur: 0 | 1
+ *
+ */
+
+int get_endianness(void)
+{
+	int x = 2;
+
+	if (x & 1)
+		return (0);
+	else
+		return (1);
+}
