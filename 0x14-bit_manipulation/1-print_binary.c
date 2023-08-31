010f070373ceefdaa18c12db68106d92781314cc
ckr25 <ckipkirui208@gmail.com>
2023-08-31 12:21:47 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -10,11 +10,11 @@ void print_binary(unsigned long int n)
 {
 	if ((n >> 1) == 0 && n == 0)
 	{
-		_putchar('0');
+		putchar('0');
 		return;
 	}
 
 	if (n >> 1 != 0)
 		print_binary(n >> 1);
-	_putchar((n & 1) + '0');
+	putchar((n & 1) + '0');
 }
